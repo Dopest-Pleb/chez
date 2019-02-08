ba23788c256791c5278e998bad1733e017149402
jonah yordy <jonahyordy@gmail.com>
2019-02-08 16:49:42 -0500

    okay

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
+const prefix = ">"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    let args = msg.content.slice(prefix).split(' ');
+    if (message.content.startsWith(">ping")) {
+        message.channel.send(args);
     }
 });
 
