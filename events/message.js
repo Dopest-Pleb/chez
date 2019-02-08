94d271477007f31013cfbcd8d1afdd7bff495bae
jonah yordy <jonahyordy@gmail.com>
2019-02-08 18:06:43 -0500

    okay

--- events/message.js
+++ events/message.js
@@ -1,6 +1,6 @@
 module.exports = (client, msg) => {
-    if (message.author.bot) return;
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    if (msg.author.bot) return;
+    if (msg.content.indexOf(client.config.prefix) !== 0) return;
 
     let args = msg.content.split(" ").splice(1);
     let command = msg.content.substring(client.config.length).split(" ")[0];
