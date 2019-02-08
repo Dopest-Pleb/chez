3b5dbbe59ef4dce4005b8218e00f27c549f5dd5c
jonah yordy <jonahyordy@gmail.com>
2019-02-08 17:36:31 -0500

    okay

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ fs.readdir("./commands/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
         if (!file.endsWith(".js")) return;
+        console.log(file);
         const command = require(`./commands/${file}`);
         client.commands.set(command.help.name, command);
     });
@@ -19,11 +20,17 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    console.log("okay");
     if (!msg.content.startsWith(prefix)) return;
+    console.log("okay");
     let args = msg.content.split(" ").splice(1);
     let command = msg.content.substring(prefix.length).split(" ");
     let cmd = client.commands.get(command);
-    if (cmd) cmd.run(client, msg, args);
+    
+    if (cmd) {
+        console.log("okay");
+        cmd.run(client, msg, args);
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
