2e69a5a7f99963659e2e3240d1ba23080144bcea
jonah yordy <jonahyordy@gmail.com>
2019-02-08 17:25:10 -0500

    okay

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ const client = new Discord.Client();
 const fs = require("fs");
 const prefix = ">"
 
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.help.name, command);
+    });
+});
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.author.bot) return;
-    let args = msg.content.slice(prefix).split(' ');
-    if (msg.content.startsWith(">ping")) {
-        msg.channel.send(args);
-    }
+    if (!msg.startsWith(prefix)) return;
+    let args = msg.content.split(" ").splice(1);
+    let command = msg.content.substring(prefix.length).split(" ");
+    let cmd = client.commands.get(command);
+    if (cmd) cmd.run(client, msg, args);
 });
 
 client.login(process.env.BOT_TOKEN);
---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+exports.run = async (bot, message, args) => {
+    message.reply(`Pong! ${bot.ping}ms`);
+  } 
+  
+  exports.help = {
+    name: 'ping'
+  }
