871dfac907f99fc77a217e2636ddab43a9755bb7
jonah yordy <jonahyordy@gmail.com>
2019-02-08 21:43:23 -0500

    okay

--- events/message.js
+++ events/message.js
@@ -1,11 +1,12 @@
 module.exports = (client, msg) => {
     if (msg.author.bot) return;
     if (msg.content.indexOf(client.config.prefix) !== 0) return;
-    console.log("okay");
+    
 
     let args = msg.content.split(" ").splice(1);
     let command = msg.content.substring(client.config.length).split(" ")[0];
     let cmd = client.commands.get(command);
+    console.log(cmd);
     
     if (!cmd) return;
 
