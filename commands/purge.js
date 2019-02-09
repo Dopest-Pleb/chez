1587356518b9f6dda1d06cc184e01788556a2148
jonah yordy <jonahyordy@gmail.com>
2019-02-09 13:49:20 -0500

    okay

--- commands/purge.js
+++ commands/purge.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
     message.channel.fetchMessages({limit : args[0]+1}).then(messages => {
         for (let message of messages.values()) {
-            message.delete().then(m => m.channel.send(`${args[0]} messages deleted.`));
+            message.delete();
         }
     });
 } 
